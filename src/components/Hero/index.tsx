42a5c2899729128fdd1f12ba086f88f6a4aac6c2
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-14 20:32:41 -0300

    style: hero responsive

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,27 +12,36 @@ export default function Hero () {
   const arrayTitle = heroTitle.split('')
 
   return (
-    <section className="flex flex-col justify-center items-center w-full bg-zinc-100">
-      <div className={`${font.className} flex flex-wrap w-full text-9xl justify-center text-primary-foreground font-thin h-[calc(50vh)] bg-primary p-8 overflow-hidden`}>
+    <section className="flex flex-col lg:flex-row justify-center items-center w-full bg-zinc-100">
+      <div className={`${font.className} flex flex-wrap w-full text-9xl justify-center text-primary-foreground font-thin h-[calc(50vh)] bg-primary p-8 overflow-hidden md:py-20 lg:w-[80vw] md:text-[9rem] md:pl-36 2xl:pl-60`}>
         {arrayTitle.map((letter, index) => (
           <span key={index}>{letter}</span>
         ))}
       </div>
-      <div className='flex flex-col w-full px justify-center items-center -mt-48 px-4 gap-6 z-10'>
-        <Image width={280} src={book} alt='Manual da marca' />
-        <p className='text-zinc-500 text-sm'>
+      <div className='flex flex-col md:flex-row w-full px justify-center items-center -mt-48 lg:mt-0 px-4 gap-6 z-10 lg:-ml-60'>
+        <Image
+          className='w-auto h-[40vh] lg:h-[50vh]'
+          width={0}
+          height={0}
+          sizes='100vw'
+          src={book}
+          alt=''
+        />
+        <div className='flex flex-col justify-center items-center md:pr-20'>
+          <p className='text-zinc-500 text-sm'>
           Disponibilizamos para download o logo e manual da marca, que possui todas as especificações
           técnicas das cores e tipologias utilizadas. Desta forma, preservamos as propriedades
           gráficas, facilitando a correta propagação, percepção e identidade da marca Ceusa.
-        </p>
-        <Link
-          href='/downloads/logo-ceusa_6.zip'
-          className='flex items-center  gap-2 rounded-none border-2 border-primary text-zinc-600 hover:text-primary hover:bg-transparent font-normal p-4'
-          download
-        >
-          Downloads
-          <ArrowRight size={18} />
-        </Link>
+          </p>
+          <Link
+            href='/downloads/logo-ceusa_6.zip'
+            className='flex w-min items-center gap-2 rounded-none border-2 border-primary text-zinc-600 hover:text-primary hover:bg-transparent font-normal p-4 mt-12'
+            download
+          >
+            Downloads
+            <ArrowRight size={18} />
+          </Link>
+        </div>
       </div>
     </section>
   )
