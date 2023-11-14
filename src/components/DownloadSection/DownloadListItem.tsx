7ef060a96977423963a77333a4d59b23232894e8
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-14 20:53:54 -0300

    style: responsive DownloadSection

--- src/components/DownloadSection/DownloadListItem.tsx
+++ src/components/DownloadSection/DownloadListItem.tsx
@@ -6,20 +6,22 @@ import logo from 'public/images/logo-white.svg'
 
 export default function DownloadListItem () {
   return (
-    <div className='flex w-full justify-center items-center'>
-      <div className='flex justify-center items-center w-12 h-12 bg-primary rounded-md'>
+    <div className='flex w-full justify-center items-center lg:bg-white lg:py-4'>
+      <div className='flex justify-center items-center w-12 h-12 bg-primary rounded-md lg:-ml-6 lg:w-24 lg:h-24'>
         <Image src={logo} alt='' />
       </div>
-      <p className='flex-1 text-center text-xs text-zinc-500 font-bold px-8'>
+      <p className='flex-1 text-center text-xs text-zinc-500 font-bold px-8 lg:text-left lg:text-lg'>
             REVISTA LANÇAMENTOS 2023
       </p>
       <Link
         href='/downloads/revista-ceusa-2023-site_66.pdf'
-        className='flex w-min h-min items-center gap-2 rounded-none border-2 border-primary text-zinc-600 hover:text-primary hover:bg-transparent font-normal py-0.5 px-2'
+        className='flex w-min h-min items-center gap-2 rounded-none border-2 border-primary text-zinc-600 hover:text-primary hover:bg-transparent font-normal py-0.5 px-2 lg:mr-12'
         download
       >
-        <p className='hidden lg:flex'>Baixar agora</p>
-        <ArrowDownToLine className='text-primary' size={18} />
+        <div className='flex items-center justify-center min-w-max w-auto p-2 gap-2'>
+          <p className='hidden lg:flex'>Baixar agora</p>
+          <ArrowDownToLine className='text-primary' size={18} />
+        </div>
       </Link>
     </div>
   )
--- src/components/DownloadSection/index.tsx
+++ src/components/DownloadSection/index.tsx
@@ -3,23 +3,27 @@ import DownloadListItem from './DownloadListItem'
 export default function DownloadSection () {
   return (
     <section className="flex flex-col w-full justify-center">
-      <div className="bg-zinc-100 py-8">
-        <h3 className="text-zinc-500 text-2xl font-medium px-6 py-8">FAÇA O DOWNLOAD DOS NOSSOS MATERIAIS INSTITUCIONAIS.</h3>
-        <div className='flex flex-col w-full justify-center items-center gap-3'>
-          <div className='relative flex w-72 h-20 justify-center items-center bg-[url(/images/folders.webp)] bg-center bg-cover before:w-full before:h-full before:bg-black before:bg-opacity-40 hover:scale-105 transition-all duration-500 uppercase' >
-            <p className='absolute text-zinc-100 text-xl font-bold'>Folders</p>
-          </div>
-          <div className='relative flex w-80 h-20 justify-center items-center bg-[url(/images/catalog.jpg)] bg-center bg-cover before:w-full before:h-full before:bg-black before:bg-opacity-40' >
-            <p className='absolute text-zinc-100 text-xl font-bold uppercase'>Catálogos</p>
-          </div>
-          <div className='relative flex w-72 h-20 justify-center items-center bg-[url(/images/manual.jpeg)] bg-center bg-cover before:w-full before:h-full before:bg-black before:bg-opacity-40 hover:scale-105 transition-all duration-500 uppercase' >
-            <p className='absolute text-zinc-100 text-xl font-bold'>Manuais</p>
+      <div className="flex flex-col w-full justify-center items-center bg-zinc-100 py-8">
+        <div className='flex flex-col justify-center items-center w-full max-w-7xl'>
+          <h3 className="text-zinc-500 text-2xl font-medium px-6 py-8 lg:text-3xl lg:py-16">
+          FAÇA O <span className='font-bold'>DOWNLOAD</span> DOS NOSSOS MATERIAIS INSTITUCIONAIS.
+          </h3>
+          <div className='flex flex-col lg:flex-row w-full justify-center items-center gap-3'>
+            <div className='relative flex w-72 h-20 justify-center items-center bg-[url(/images/folders.webp)] bg-center bg-cover before:w-full before:h-full before:bg-black before:bg-opacity-40 hover:scale-105 transition-all duration-500 uppercase lg:w-[30vw] lg:h-36' >
+              <p className='absolute text-zinc-100 text-xl font-bold'>Folders</p>
+            </div>
+            <div className='relative flex w-80 h-20 justify-center items-center bg-[url(/images/catalog.jpg)] bg-center bg-cover before:w-full before:h-full before:bg-black before:bg-opacity-40 lg:w-[30vw] lg:h-40' >
+              <p className='absolute text-zinc-100 text-xl font-bold uppercase'>Catálogos</p>
+            </div>
+            <div className='relative flex w-72 h-20 justify-center items-center bg-[url(/images/manual.jpeg)] bg-center bg-cover before:w-full before:h-full before:bg-black before:bg-opacity-40 hover:scale-105 transition-all duration-500 uppercase lg:w-[30vw] lg:h-36' >
+              <p className='absolute text-zinc-100 text-xl font-bold'>Manuais</p>
+            </div>
           </div>
         </div>
       </div>
-      <div className='flex flex-col justify-center items-center w-full p-8'>
+      <div className='flex flex-col justify-center items-center w-full p-8 lg:bg-zinc-100'>
         <h3 className='text-zinc-500 pb-8'>Faça o download dos catálogos Ceusa.</h3>
-        <div className='w-full space-y-5'>
+        <div className='w-full lg:max-w-5xl space-y-5'>
           <DownloadListItem />
           <DownloadListItem />
           <DownloadListItem />
